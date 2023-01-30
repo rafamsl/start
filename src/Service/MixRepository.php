753a16dfb8d69a71cf2f939cab459103f43387f9
rafamsl <rafael.msl81@gmail.com>
2023-01-30 22:39:13 +0000

    local env

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -16,7 +16,7 @@ class MixRepository
     public function findAll(): array{
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter(5);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->httpClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
     }
